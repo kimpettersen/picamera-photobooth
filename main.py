dca19437c22723c6f79d992b5e69895daf3460d2
= <=>
2019-09-06 14:32:45 +0200

    preview photo after capture

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+
 from picamera import PiCamera, Color
 from time import sleep
 from datetime import datetime
+from PIL import Image
 
 import RPi.GPIO as GPIO
 import numpy as np
@@ -10,18 +12,17 @@ import os
 
 class Photobooth:
     def __init__(self):
-	self.picture_count = 1
+        self.picture_count = 1
 	self.event_name = "bryllup"
 
 	self.base_path  = "/home/pi/photobooth/google-photos-uploader/" + self.event_name
 	self.assert_path_exist()	
 	self.width = 1280
-	self.height = 720
+	self.height = 960
 
         self.flash_overlay = np.zeros((self.width, self.height, 3), dtype=np.uint8)
 	self.flash_overlay[:, :, :] = 0xff
-	self.flash_overlay[:, :, :] = 0xff	
-		
+	self.flash_overlay[:, :, :] = 0xff
 	self.camera = PiCamera()
 
 	# xdpyinfo  | grep 'dimensions:'
@@ -30,8 +31,7 @@ class Photobooth:
 	self.camera.framerate = 15
 	self.camera.image_effect = 'denoise'
 	self.camera.brightness = 50
-		
-		
+
 	# TODO: consider using board for wider Pi support
 	# https://raspberrypi.stackexchange.com/questions/12966/what-is-the-difference-between-board-and-bcm-for-gpio-pin-numbering
 	GPIO.setmode(GPIO.BCM)
@@ -47,20 +47,30 @@ class Photobooth:
 			photobooth.start_countdown()
 			photobooth.capture()
 	self.stop()
-    
+
     def stop(self):
 	self.camera.stop_preview()
 	self.camera.close()
-		
-	
+
     def capture(self):
 	dt = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
-	filename = self.base_path + "/" + self.event_name + "-" + dt + ".jpg"
-	
+        filename = self.base_path + "/" + self.event_name + "-" + dt + ".jpg"
 	o = self.camera.add_overlay(np.getbuffer(self.flash_overlay), layer=3, alpha=200)
-	sleep(.1)
+        sleep(.1)
+        self.camera.remove_overlay(o)
+        self.camera.capture(filename, format="jpeg", quality=100, thumbnail=(60, 45, 35))
+	img = Image.open(filename)
+	pad = Image.new('RGB', (
+		((img.size[0] + 31) // 32) * 32,
+		((img.size[1] + 15) // 16) * 16,
+        ))
+	pad.paste(img, (0, 0))
+
+	o = self.camera.add_overlay(pad.tobytes(), size=img.size)
+	o.layer = 3
+	o.layer = 3
+	sleep(5)
 	self.camera.remove_overlay(o)
-	self.camera.capture(filename)
 	
 
     def start_button_listener(self):
@@ -68,28 +78,28 @@ class Photobooth:
 		# wait until button is pressed
 		if GPIO.input(15) == GPIO.HIGH:
 			return
-		
+
     def start_countdown(self):
 	sleep(1)
-	
+
 	# red light
 	GPIO.setup(17,GPIO.OUT)
 	GPIO.output(17,GPIO.HIGH)
 	sleep(1)
 	GPIO.output(17,GPIO.LOW)
-	
+
 	# yellow ligth
 	GPIO.setup(18,GPIO.OUT)
 	GPIO.output(18,GPIO.HIGH)
 	sleep(1)
 	GPIO.output(18,GPIO.LOW)
-	
+
 	# green light
 	GPIO.setup(27,GPIO.OUT)
 	GPIO.output(27,GPIO.HIGH)
 	sleep(1)
 	GPIO.output(27,GPIO.LOW)
-	
+
     def assert_path_exist(self):
            assert (os.path.exists(self.base_path) is True), "path does not exist: " + self.base_path
 
@@ -97,6 +107,7 @@ if __name__ == "__main__":
     photobooth = Photobooth()
     photobooth.start()
 
+wq
 """
 TODO
 
